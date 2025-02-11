7fcee63a795c4f365e9103d9c3b0f4580d10cf4f
Sin.k <miraelsink@gmail.com>
2025-02-11 15:06:22 +0300

    almost operation done for the camera

--- src/setup/setup_camera.c
+++ src/setup/setup_camera.c
@@ -6,16 +6,16 @@
 /*   By: maandria <maandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:58:32 by maandria          #+#    #+#             */
-/*   Updated: 2025/02/05 14:51:38 by maandria         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:23:13 by maandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
 
-t_viewpoint	mr_camera_init(float n, t_prog *prog)
+t_viewport	mr_camera_init(float n, t_prog *prog)
 {
 	t_object	*tmp;
-	t_viewpoint	view;
+	t_viewport	view;
 	float		ratio_px;
 	float		angle;
 
@@ -27,3 +27,12 @@ t_viewpoint	mr_camera_init(float n, t_prog *prog)
 	return (view);
 }
 
+t_coord	mr_pixel_position(t_prog *prog, t_viewport view, int x, int y)
+{
+	t_coord		orient_cam;
+	t_coord		position_px;
+	t_object	position_cam;
+
+	position_cam = find_id(prog, "C");
+	orient_cam = position_cam.coord;
+}

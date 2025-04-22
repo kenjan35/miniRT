1e1ba50c99dd8fc7195ab52fbefcac8214765b71
Andriniaina Fernand Tolojanahary <atolojan@c1r8s13.42antananarivo.mg>
2025-04-22 13:56:42 +0300

    finish rendering

--- src/image/ray_shape.c
+++ src/image/ray_shape.c
@@ -121,32 +121,38 @@ double	get_real_time(t_prog *prog, t_viewport *view, double *xy, t_ray *ray)
 	return (time[0]);
 }*/
 
-double	set_shape_time(t_coord *px_position, t_prog *prog, t_object *obj, t_ray *ray)
+double	set_shape_time(t_coord *px_position, t_prog *prog, t_object *obj)
 {
 	double	time;
+	t_ray	ray;
+	t_coord		rt;
 
 	time = INFINITY;
 	if (obj->id[1] == 'l')
 	{
-		*ray = op_quadrique_value_pl(*px_position, prog);
-		time = time_inter_pl(ray, obj);
+		ray = op_quadrique_value_pl(*px_position, prog);
+		time = time_inter_pl(&ray, obj);
 	}
 	else if (obj->id[1] == 'y')
 	{
-		*ray = op_quadrique_value_cy(*px_position, prog, obj);
-		time = inter_cy(ray, obj);
+		ray = op_quadrique_value_cy(*px_position, prog, obj);
+		time = inter_cy(&ray, obj);
+		rt = ray_launch(ray.ro, ray.v, time);
+		if (get_extremity(&(ray.ro), &rt, obj) == 1)
+			return (time);
+		else
+			return (0);
 	}
 	else if (obj->id[1] == 'p')
 	{
-		*ray = op_quadrique_value_sp(*px_position, prog, obj);
-		time = inter_sp(ray, obj);
+		ray = op_quadrique_value_sp(*px_position, prog, obj);
+		time = inter_sp(&ray, obj);
 	}
 	return (time);
 }
 
-t_object	*get_closest(t_prog *prog, t_viewport *view, double *xy, t_ray *ray)
+t_object	*get_closest(t_prog *prog, t_viewport *view, double *xy)
 {
-	t_coord		px_position;
 	t_object	*obj;
 	double		time;
 	double		obj_time;
@@ -155,19 +161,23 @@ t_object	*get_closest(t_prog *prog, t_viewport *view, double *xy, t_ray *ray)
 	time = INFINITY;
 	obj_time = INFINITY;
 	obj = NULL;
-	px_position = mr_pixel_position(prog, *view, xy);
+	prog->px_pos = mr_pixel_position(prog, *view, xy);
 	tmp = prog->shapes;
 	while (tmp != NULL)
 	{
 		obj = (t_object *) (tmp->content);
-		obj_time = set_shape_time(&px_position, prog, obj, ray);
+		obj_time = set_shape_time(&(prog->px_pos), prog, obj);
 		obj->time = obj_time;
-		if (obj->time < time)
+		if (obj->time != INFINITY && obj->time > 0)
 		{
-			time = obj->time;
-			prog->current_cy = obj;
+			if (obj->time < time)
+			{
+				time = obj->time;
+				prog->current_cy = obj;
+			}
 		}
 		tmp = tmp->next;
 	}
+	obj = prog->current_cy;
 	return (obj);
 }

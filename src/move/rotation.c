81f543e654ab92ba36f9b96f9de817914f8b3cdf
Andriniaina Fernand Tolojanahary <atolojan@c1r8s13.42antananarivo.mg>
2025-05-12 09:01:18 +0300

    normalize all rotation move

--- src/move/rotation.c
+++ src/move/rotation.c
@@ -16,33 +16,54 @@ void	rotate_x(t_object *obj, double angle)
 {
 	double	y;
 	double	z;
+	t_coord	to_coord;
+	double	norm;
 
 	y = obj->orient->or_y * cos(angle) - obj->orient->or_z * sin(angle);
 	z = obj->orient->or_y * sin(angle) + obj->orient->or_z * cos(angle);
 	obj->orient->or_y = y;
 	obj->orient->or_z = z;
+	to_coord = orient2coord(obj->orient);
+	norm = op_norm(to_coord);
+	obj->orient->or_x /= norm;
+	obj->orient->or_y /= norm;
+	obj->orient->or_z /= norm;
 }
 
 void	rotate_y(t_object *obj, double angle)
 {
 	double	x;
 	double	z;
+	t_coord	to_coord;
+	double	norm;
 
 	x = obj->orient->or_x * cos(angle) + obj->orient->or_z * sin(angle);
 	z = -(obj->orient->or_x * sin(angle)) + obj->orient->or_z * cos(angle);
 	obj->orient->or_x = x;
 	obj->orient->or_z = z;
+	to_coord = orient2coord(obj->orient);
+	norm = op_norm(to_coord);
+	obj->orient->or_x /= norm;
+	obj->orient->or_y /= norm;
+	obj->orient->or_z /= norm;
 }
 
 void	rotate_z(t_object *obj, double angle)
 {
 	double	x;
 	double	y;
+	t_coord	to_coord;
+	double	norm;
 
 	x = obj->orient->or_x * cos(angle) - obj->orient->or_y * sin(angle);
 	y = obj->orient->or_x * sin(angle) + obj->orient->or_y * cos(angle);
 	obj->orient->or_x = x;
 	obj->orient->or_y = y;
+	to_coord = orient2coord(obj->orient);
+	norm = op_norm(to_coord);
+	obj->orient->or_x /= norm;
+	obj->orient->or_y /= norm;
+	obj->orient->or_z /= norm;
 }
 
 int	rotate(int key, t_prog *prog)
